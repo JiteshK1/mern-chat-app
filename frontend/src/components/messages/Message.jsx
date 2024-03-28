40461b8750b665ef1b284efc6a4341f7a1ad1389
JiteshK1 <kanyaljitesh0@gmail.com>
2024-03-28 22:54:09 +0530

    Design

---
+++ frontend/src/components/messages/Message.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Message = () => {
+  return (
+    <>
+    <div>Hiii</div>
+    <div>Hiii</div>
+    <div>Hiii</div>
+    <div>Hiii</div>
+    <div>Hiii</div>
+    <div>Hiii</div>
+    <div>Hiii</div>
+    <div>Hiii</div>
+    <div>Hiii</div>
+    <div>Hiii</div>
+    </>
+  )
+}
+
+export default Message
---
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -0,0 +1,11 @@
+import { BiLogOut } from "react-icons/bi";
+
+const LogoutButton = () => {
+
+	return (
+		<div className='mt-auto'>
+			lgout
+		</div>
+	);
+};
+export default LogoutButton;
